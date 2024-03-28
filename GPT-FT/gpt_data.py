0aa8c11373e232acb6441592f028a06d6d6cc962
黄柏特 <471314513@qq.com>
2024-03-28 19:18:41 +0800

    docs:add gpt-FT format dataset fix

--- GPT-FT/gpt_data.py
+++ GPT-FT/gpt_data.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-csv_file = 'set/dev.csv'
-jsonl_file = 'gpt_dev.jsonl'
+csv_file = 'set/train.csv'
+jsonl_file = 'GPT-FT/gpt_train.jsonl'
 
 # 生成JSONL文件
 messages = []
@@ -15,8 +15,8 @@ with open(csv_file, 'r', encoding='utf-8') as file:
         if len(row) >= 4:
             sentence = row[0]
             topic = row[1]
-            score = float(row[3])
-            message={"messages": [{"role": "system", "content": "请根据以下论述和主题,给出论证质量评分(只需要输出一个范围从0-1的分数,精确到小数点后9位)."}, {"role": "user", "content": {'sentence':sentence,'topic': topic}}, {"role": "assistant", "content": score}]}
+            score = row[3]
+            message={"messages": [{"role": "system", "content": "请根据以下论述和主题,给出论证质量评分(只需要输出一个范围从0-1的分数,精确到小数点后9位)."}, {"role": "user", "content": str({'sentence':{sentence},'topic': {topic}})}, {"role": "assistant", "content": score}]}
             messages.append(message)
 # 保存为JSONL文件
 with open(jsonl_file, 'w', encoding='utf-8') as file:
