50ba28e96c39565995356b08269a63d49e3e10ab
黄柏特 <471314513@qq.com>
2024-03-28 21:05:30 +0800

    docs:add LLAMA-FT data

--- LLAMA-FT/llama_data.py
+++ LLAMA-FT/llama_data.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-csv_file = 'set/dev.csv'
-jsonl_file = 'LLAMA-FT/spark_dev.jsonl'
+csv_file = 'set/test.csv'
+jsonl_file = 'LLAMA-FT/spark_test.jsonl'
 
 # 生成JSONL文件
 messages = []
@@ -16,7 +16,7 @@ with open(csv_file, 'r', encoding='utf-8') as file:
             sentence = row[0]
             topic = row[1]
             score = row[3]
-            message={ "instruction":"请根据以下论述和主题,给出论证质量评分(只需要输出一个范围从0-1的分数,精确到小数点后9位).","input": str({'sentence':{sentence},'topic': {topic}}),"target":score}
+            message={ "instruction":"请根据以下论述和主题,给出论证质量评分(只需要输出一个范围从0-1的分数,精确到小数点后9位).","input": str({'sentence':{sentence},'topic': {topic}}),"output":score}
             messages.append(message)
 # 保存为JSONL文件
 with open(jsonl_file, 'w', encoding='utf-8') as file:
