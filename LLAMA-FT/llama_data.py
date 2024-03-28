8378ac017ff47afa227925bd6b554f61ef2000c1
黄柏特 <471314513@qq.com>
2024-03-28 21:04:04 +0800

    docs:add LLAMA-FT data

---
+++ LLAMA-FT/llama_data.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+csv_file = 'set/dev.csv'
+jsonl_file = 'LLAMA-FT/spark_dev.jsonl'
+
+# 生成JSONL文件
+messages = []
+
+with open(csv_file, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  # 跳过标题行
+
+    for row in reader:
+        if len(row) >= 4:
+            sentence = row[0]
+            topic = row[1]
+            score = row[3]
+            message={ "instruction":"请根据以下论述和主题,给出论证质量评分(只需要输出一个范围从0-1的分数,精确到小数点后9位).","input": str({'sentence':{sentence},'topic': {topic}}),"target":score}
+            messages.append(message)
+# 保存为JSONL文件
+with open(jsonl_file, 'w', encoding='utf-8') as file:
+    for message in messages:
+        file.write(json.dumps(message, ensure_ascii=False) + '\n')
+        
